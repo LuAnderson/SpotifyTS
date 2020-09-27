a3307d5dd857c862e7e33d1994966fe8993b7364
LuAnderson <lucasand96@gmail.com>
2020-09-27 02:35:50 -0300

    feat(*): create service of the playlist request and add to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import * as FilterService from '../services/filters';
+import * as PlaylistService from '../services/playlists';
 import { IFilterResponse } from '../interfaces';
-
-
-// import { Card } from '../components';
 
 function HomePage() {
 
     const [responseFilter, setResponseFilter] = useState<IFilterResponse>();
+    const [responsePlaylist, setResponsePlaylist] = useState();
 
     useEffect(() => {
         const _getFilters = async () => {
             const response : IFilterResponse = await FilterService.getFilters();
             setResponseFilter(response);
         }
+
+        const _getPlaylists = async () => {
+            const response = await PlaylistService.getPlaylists();
+            setResponsePlaylist(response);
+        }
+        _getPlaylists();
         _getFilters();
     }, []);
 
---
+++ src/services/playlists/index.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+// import { IFilterResponse } from '../../interfaces';
+
+const headers : object = {
+    Authorization: 'Bearer BQDb0EfUzN01P0AzCayXN3q0S-YpNVu8s_5BVZlMot0pV3uKzQS8_JlCJX9xCaHVGxsimo_pYMe-4V-QAxFuUjfHuM8h7uhuWzzEQMoNtY0X1J3xdKw831IbSRv9m8GE9d_vyt3v6Kveb0-iCzks7rwKA05lYvdKMzjSGRc-Y171a1qrnR-2Dw2oD1QHr7Hr_eqPdEZl0co'
+};
+
+const getPlaylists = async () => (
+    (await axios.get('https://api.spotify.com/v1/browse/featured-playlists', { headers: headers })).data
+);
+
+export { getPlaylists };
