68dcc44442121d303d4aad9a9aca64e37f9c4ecc
LuAnderson <lucasand96@gmail.com>
2020-09-27 02:16:03 -0300

    feat(home.p): make the requisition of the api of filters using interface

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import * as FilterService from '../services/filters';
+// import { Card } from '../components';
+
+interface IFilterResponse {
+    filters: IFilters[]
+};
+
+interface IFilters {
+    id: string;
+    name: string;
+    values?: Array<object>,
+    validation?: Array<object>
+};
 
 function HomePage() {
-  return (
-    <p>Teste de Component</p>
-  );
+
+    const [responseFilter, setResponseFilter] = useState<IFilterResponse>();
+
+    useEffect(() => {
+        const _getFilters = async () => {
+            const response : IFilterResponse = await FilterService.getFilters();
+            setResponseFilter(response);
+        }
+        _getFilters();
+    }, []);
+
+    return (
+        <>
+           HomePage App
+        </>
+    );
 }
 
 export default HomePage;
